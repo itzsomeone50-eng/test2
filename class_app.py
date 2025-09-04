002e829c8e306d99cfd5c50a502ffc69bbc4404e
itzsomeone50-eng <itzsomeone50@gmail.com>
2025-09-05 04:26:13 +0500

    class_app.py

--- class_app.py
+++ class_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 # Load pre-trained sentiment model
 with open("model_class.pkl", "rb") as file:
     model = pickle.load(file)
@@ -9,10 +11,11 @@ st.title("📌 Sentiment Analyzer")
 
 # Text input
 user_text = st.text_input("Enter your text:")
-
+vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)
 # Button
 if st.button("Check Sentiment"):
     if user_text:
+        user_text = vectorizer.transform(user_text)
         prediction = model.predict([user_text])[0]
         st.write(f"**Prediction:** {prediction}")
     else:
